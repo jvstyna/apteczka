2fac4a9e878aac910a44da3aeb3bbf46a192675d
jvstyna <j.krzywdziak@gmail.com>
2022-05-09 22:13:08 +0200

    add taken pills form

---
+++ zazycie.php
@@ -0,0 +1,17 @@
+<?php 
+session_start();
+?> 
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>...:::Zażywanie:::...</title>
+</head>
+<body>
+    <form method="POST" action="./zazyj.php">
+        <input type="number" name="IdLeku" value="<?php echo $_GET['ajdi']; ?> "><br>
+        <input type="number" name="Ilosc" min="0" max="<?php echo $_GET['ile']; ?>" placeholder="Ilość"><br>
+        <input type="submit" name="submit" value="Zażyj">
+</form>
+</body>
+</html>
