1147aebd50668f001ad125a9b2f8d84001d7cac9
jvstyna <j.krzywdziak@gmail.com>
2022-05-09 23:14:51 +0200

    fixing small bugs

--- zazycie.php
+++ zazycie.php
@@ -9,8 +9,8 @@ session_start();
 </head>
 <body>
     <form method="POST" action="./zazyj.php">
-        <input type="number" name="IdLeku" value="<?php echo $_GET['ajdi']; ?> "><br>
-        <input type="number" name="Ilosc" min="0" max="<?php echo $_GET['ile']; ?>" placeholder="Ilość"><br>
+        <input type="number" name="IdLeku" value="<?php echo $_GET['ajdi']; ?>" placeholder="Nazwa"><br>
+        <input type="number" name="Ilosc" min="1" max="<?php echo $_GET['ile']; ?>" placeholder="Ilość"><br>
         <input type="submit" name="submit" value="Zażyj">
 </form>
 </body>
